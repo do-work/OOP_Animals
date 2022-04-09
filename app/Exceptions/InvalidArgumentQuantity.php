21fc59cde3cec2862384e02d8758815a1bb03ce0
dolson <deolson87@gmail.com>
2022-04-08 20:40:41 -0400

    Simplify custom exceptions.

--- app/Exceptions/InvalidArgumentQuantity.php
+++ app/Exceptions/InvalidArgumentQuantity.php
@@ -2,16 +2,9 @@
 
 namespace App\Exceptions;
 
-
 use Exception;
-use Throwable;
-
 
 class InvalidArgumentQuantity extends Exception
 {
 
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
--- app/Exceptions/MissingAnimalException.php
+++ app/Exceptions/MissingAnimalException.php
@@ -3,12 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
-use Throwable;
 
 class MissingAnimalException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+
 }
